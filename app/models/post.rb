20801df64f1aac95334458732d6a85bc82fba3a6
Kevin Trinh <kxt.trinh@gmail.com>
2023-03-06 16:17:07 +0100

    creating 2nd test

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
   has_many :comments
+
+  validates :title, :description, presence: true
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,5 +4,10 @@ RSpec.describe Post, type: :model do
   describe 'associations' do
     it { is_expected.to have_many(:comments) }
   end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:description) }
+  end
   
 end
