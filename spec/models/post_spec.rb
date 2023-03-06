f1ae9a55fe0ff86a4b42e824efa5bc286afa9b7c
Kevin Trinh <kxt.trinh@gmail.com>
2023-03-06 16:06:51 +0100

    creating first test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to have_many(:comments) }
+  end
+  
 end
